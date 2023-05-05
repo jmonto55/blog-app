f675abf9cd94775dfaed9ca65557db22340e1eb4
jmonto55 <jmonto55@gmail.com>
2023-05-05 01:13:17 -0500

    Fix rubocop offenses

--- db/migrate/20230505053200_create_posts.rb
+++ db/migrate/20230505053200_create_posts.rb
@@ -1,6 +1,11 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
+      t.string :title
+      t.text :text
+      t.integer :comments_counter
+      t.integer :likes_counter
+      t.references :author, foreign_key: { to_table: 'users' }
 
       t.timestamps
     end
