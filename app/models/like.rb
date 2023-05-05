7ee84c55b7d982e82d774d28dfab3775fccaea4d
jmonto55 <jmonto55@gmail.com>
2023-05-05 01:06:12 -0500

    Add migration files to create tables

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,3 @@
 class Like < ApplicationRecord
+  belongs_to :post
 end
--- db/migrate/20230505053213_create_users.rb
+++ db/migrate/20230505053213_create_users.rb
@@ -1,6 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
+      t.string :name
+      t.string :photo
+      t.text :bio
+      t.integer :posts_counter
 
       t.timestamps
     end
