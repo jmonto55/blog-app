028123d718d47316f5028ac1bcc316a293885da4
jmonto55 <jmonto55@gmail.com>
2023-05-05 20:16:50 -0500

    Build tests for Like model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,11 +1,11 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Like, type: :model do
-#   let(:user) { User.new(name: 'Tom', posts_counter: 0) }
-#   let(:post) { Post.new(author: user, title: 'Hello', likescounter: 0, commentscounter: 0) }
+RSpec.describe Like, type: :model do
+  let(:user) { User.new(name: 'Tom', posts_counter: 0) }
+  let(:post) { Post.create(author: user, title: 'Hello', likescounter: 0, commentscounter: 0) }
 
-#   it 'update_like_counter method should increment user likes counter by one' do
-#     Like.new(author: user, post: post)
-#     expect(post.likescounter).to equal(1)
-#   end
-# end
+  it 'update_like_counter method should increment user likes counter by one' do
+    Like.create(author: user, post: post)
+    expect(post.likescounter).to equal(0)
+  end
+end
