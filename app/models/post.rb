4a74f5dbc2c1a803ed01cf2ab394ba01d7fbe728
jmonto55 <jmonto55@gmail.com>
2023-05-11 18:43:41 -0500

    Add seed data and users view

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,11 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { in: 2..250 }
   validates :comments_counter, comparison: { greater_than_or_equal_to: 0 }
   validates :likes_counter, comparison: { greater_than_or_equal_to: 0 }
-
+  attribute :likes_counter, :integer, default: 0
+  attribute :comments_counter, :integer, default: 0
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
-  has_many :comments
-  has_many :likes
+  has_many :comments, foreign_key: :post_id
+  has_many :likes, foreign_key: :post_id
 
   after_save :update_post_counter
 
