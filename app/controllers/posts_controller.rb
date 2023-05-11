67d16bf3f773b8667c59f60aef82e0dbaa654d00
jmonto55 <jmonto55@gmail.com>
2023-05-11 18:53:15 -0500

    Build posts view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = Post.where(posts: { author_id: params[:user_id] })
+    @posts = Post.where(posts: { user_id: params[:user_id] })
   end
 
   def show
