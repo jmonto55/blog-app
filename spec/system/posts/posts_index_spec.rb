282cb76561a182faa2eb92fef22dca7061e3a8b8
jmonto55 <jmonto55@gmail.com>
2023-05-16 13:12:44 -0500

    Fix rubocop offenses

--- spec/system/posts/posts_index_spec.rb
+++ spec/system/posts/posts_index_spec.rb
@@ -8,48 +8,41 @@ RSpec.describe 'Posts index page', type: :system do
       @comment1 = Comment.create(text: 'My first comment', author: @user, post: @post)
       @comment2 = Comment.create(text: 'My second comment', author: @user, post: @post)
     end
+    background { visit user_posts_path(@user.id) }
 
-    feature 'Post index page' do
-      background { visit user_posts_path(@user.id) }
+    it 'I can see the user\'s profile picture.' do
+      img_src = page.first('img')['src']
+      expect(img_src).to have_content 'pic-374e2a3927f9388c669fa74572e0c3261b8a656597b600535b14bdd49399236a.jpg'
+    end
 
-      it 'I can see the user\'s profile picture.' do
-        img_src = page.first('img')['src']
-        expect(img_src).to have_content 'pic-374e2a3927f9388c669fa74572e0c3261b8a656597b600535b14bdd49399236a.jpg'
-      end
+    it 'I can see the number of posts the user has written' do
+      expect(page).to have_content('Number of posts: 1')
+    end
 
-      it 'I can see the number of posts the user has written' do
-        expect(page).to have_content('Number of posts: 1')
-      end
+    it 'I can see some of the post\'s body' do
+      expect(page).to have_content 'testing'
+    end
 
-      it 'I can see some of the post\'s body' do
-        expect(page).to have_content 'testing'
-      end
+    it 'I can see the first comments on a post.' do
+      expect(page).to have_content 'My first comment'
+    end
 
-      it 'I can see the first comments on a post.' do
-        expect(page).to have_content 'My first comment'
-      end
-
-      it 'I can see the user\'s username.' do
-        expect(page).to have_content 'Jose'
-      end
-
-      it 'I can see how many comments a post has.' do
-        expect(page).to have_content 'Comments: 7'
-      end
-
-      it 'I can see how many likes a post has.' do
-        expect(page).to have_content '❤️ 7'
-      end
-
-      it 'if comments counter is integer' do
-        @post.comments_counter = 8
-        expect(@post).to be_valid
-      end
-
-      it 'When I click on a post, it redirects me to that post\'s show page.' do
-        click_link 'testing'
-        expect(page).to have_content("Post #{@post.id} by Jose")
-      end
+    it 'I can see the user\'s username.' do
+      expect(page).to have_content 'Jose'
+    end
+    it 'I can see how many comments a post has.' do
+      expect(page).to have_content 'Comments: 7'
+    end
+    it 'I can see how many likes a post has.' do
+      expect(page).to have_content '❤️ 7'
+    end
+    it 'if comments counter is integer' do
+      @post.comments_counter = 8
+      expect(@post).to be_valid
+    end
+    it 'When I click on a post, it redirects me to that post\'s show page.' do
+      click_link 'testing'
+      expect(page).to have_content("Post #{@post.id} by Jose")
     end
   end
 end
